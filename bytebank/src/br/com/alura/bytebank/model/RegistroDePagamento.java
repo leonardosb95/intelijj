902ca35a6801f3d47b376d4a333e485b8820e0ef
Leonardo dos Santos Bispo <leonardosantosbispo@gmail.com>
2020-08-10 09:46:50 -0300

    Aula 04 finalizada

--- bytebank/src/br/com/alura/bytebank/model/RegistroDePagamento.java
+++ bytebank/src/br/com/alura/bytebank/model/RegistroDePagamento.java
@@ -1,22 +1,23 @@
 package br.com.alura.bytebank.model;
 
+import br.com.alura.bytebank.servico.ServicoPagamento;
+import br.com.alura.bytebank.validador.ValidaPagamento;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class RegistroDePagamento {
     private final static List<Pagamento> PAGAMENTOS = new ArrayList<>();
+    private ServicoPagamento servico= new ServicoPagamento();
+
 
     public void registra(List<Pagamento> pagamentos) {
-        for (int i = 0; i < pagamentos.size(); i++) {
-            Pagamento pagamento = pagamentos.get(i);
-            verificaTipo(pagamento);
-            salva(pagamento);
-        }
+        servico.registra(pagamentos);
         exibeEfetuados();
     }
 
-    private void exibeEfetuados() {
+    public void exibeEfetuados() {
         if (!PAGAMENTOS.isEmpty()) {
             System.out.println("Todos os pagamentos");
             for (int i = 0; i < PAGAMENTOS.size(); i++) {
@@ -27,14 +28,7 @@ public class RegistroDePagamento {
         }
     }
 
-    private void verificaTipo(Pagamento pagamento) {
-        if (pagamento.getTipo().equals(Tipo.CREDITO)) {
-            pagamento.setValor(pagamento.getValor() * 1.02);
-        }
-        if (pagamento.getTipo().equals(Tipo.DEBITO)) {
-            pagamento.setValor(pagamento.getValor() * 1.001);
-        }
-    }
+
 
     private void salva(Pagamento pagamento) {
         PAGAMENTOS.add(pagamento);
---
+++ bytebank/src/br/com/alura/bytebank/servico/ServicoPagamento.java
@@ -0,0 +1,29 @@
+package br.com.alura.bytebank.servico;
+
+import br.com.alura.bytebank.model.Pagamento;
+import br.com.alura.bytebank.model.RegistroDePagamento;
+import br.com.alura.bytebank.validador.ValidaPagamento;
+
+import java.util.List;
+
+public class ServicoPagamento {
+
+    RegistroDePagamento registro= new RegistroDePagamento();
+
+
+    public void registra(List<Pagamento> pagamentos) {
+        ValidaPagamento validador = new ValidaPagamento();
+        validador.verificaTipo(pagamentos);
+        registro.registra(pagamentos);
+    }
+
+    public void exibeEfetuados() {
+        registro.exibeEfetuados();
+    }
+
+
+
+
+
+
+}
